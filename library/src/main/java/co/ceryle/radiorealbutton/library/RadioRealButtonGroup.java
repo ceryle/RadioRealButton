b0079604c116ddb000154d0eb5ebc4a9a804611e
EGE <egeaker@gmail.com>
2017-04-28 17:58:50 +0300

    fixed issue: calling deselect() twice in a row reselects old value

--- library/src/main/java/co/ceryle/radiorealbutton/library/RadioRealButtonGroup.java
+++ library/src/main/java/co/ceryle/radiorealbutton/library/RadioRealButtonGroup.java
@@ -608,10 +608,13 @@ public class RadioRealButtonGroup extends RoundedCornerLayout {
     }
 
     public void deselect() {
+        if(!buttons.get(lastPosition).isChecked())
+            return;
+
         if (animationType == ANIM_TRANSLATE_X) {
             deselect(lastPosition, true);
         } else {
-            if (lastPosition != -1 && buttons.get(lastPosition).isChecked()) {
+            if (lastPosition != -1) {
                 setPosition(lastPosition);
             }
         }
