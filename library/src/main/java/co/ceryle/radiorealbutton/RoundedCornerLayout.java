1ff037a301dc702b3099377b8183a20a58c31698
Vadiraj Purohit <deus.ex.machina@redmorph.com>
2018-08-14 09:25:03 -0400

    Fixed the radio real button appearance for Android P with Region.Op.INTERSECT

--- library/src/main/java/co/ceryle/radiorealbutton/RoundedCornerLayout.java
+++ library/src/main/java/co/ceryle/radiorealbutton/RoundedCornerLayout.java
@@ -78,8 +78,8 @@ public class RoundedCornerLayout extends FrameLayout {
         final Path path = new Path();
         path.addRoundRect(new RectF(0, 0, canvas.getWidth(), canvas.getHeight()), cornerRadius, cornerRadius, Path.Direction.CW);
         // https://developer.android.com/reference/android/graphics/Canvas#clipPath(android.graphics.Path,%20android.graphics.Region.Op)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            canvas.clipOutPath(path);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            canvas.clipPath(path, Region.Op.INTERSECT);
         } else {
             canvas.clipPath(path, Region.Op.REPLACE);
         }
